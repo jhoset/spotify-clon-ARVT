1611a5a918e4d8adaed8199058d542407cb60962
Jhoset NG <adm_dng@hotmail.com>
2024-02-11 21:18:08 -0500

    Design Main Layout with Astro-Svelte-React Components

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    let greeting = "";
+    if (currentHour < 12) {
+        greeting = "Good Morning :D";
+    } else if (currentHour < 18) {
+        greeting = "Good Afternoon :D";
+    } else {
+        greeting = "Good Evening :D";
+    }
+</script>
+
+<h1 class="text-3xl font-bold pt-10">{greeting}</h1>
