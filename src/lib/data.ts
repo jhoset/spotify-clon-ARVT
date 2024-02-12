6ed117db455ab2254cff6a022e57c7c3e391a832
Jhoset NG <adm_dng@hotmail.com>
2024-02-12 18:01:09 -0500

    Remove Unnecesary Files

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -148,42 +148,6 @@ export const songs: Song[] = [
         "duration": "3:40"
     },
     {
-        "id": 2,
-        "albumId": 2,
-        "title": "Lost Pages",
-        "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
-        "artists": ["Urban Nocturne"],
-        "album": "Midnight Tales",
-        "duration": "3:20"
-    },
-    {
-        "id": 3,
-        "albumId": 2,
-        "title": "Midnight Tales",
-        "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
-        "artists": ["Urban Nocturne"],
-        "album": "Midnight Tales",
-        "duration": "3:50"
-    },
-    {
-        "id": 4,
-        "albumId": 2,
-        "title": "City Lights",
-        "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
-        "artists": ["Urban Nocturne"],
-        "album": "Midnight Tales",
-        "duration": "3:30"
-    },
-    {
-        "id": 5,
-        "albumId": 2,
-        "title": "Night Drive",
-        "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
-        "artists": ["Urban Nocturne"],
-        "album": "Midnight Tales",
-        "duration": "4:20"
-    },
-    {
         "id": 1,
         "albumId": 3,
         "title": "Lunar",
@@ -193,42 +157,6 @@ export const songs: Song[] = [
         "duration": "3:40"
     },
     {
-        "id": 2,
-        "albumId": 3,
-        "title": "Go go go!",
-        "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "3:20"
-    },
-    {
-        "id": 3,
-        "albumId": 3,
-        "title": "Keep focus!",
-        "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "2:40"
-    },
-    {
-        "id": 4,
-        "albumId": 3,
-        "title": "JavaScript is the way",
-        "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "3:10"
-    },
-    {
-        "id": 5,
-        "albumId": 3,
-        "title": "No more TypeScript for me",
-        "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "2:10"
-    },
-    {
         "id": 1,
         "albumId": 4,
         "title": "Lunar",
@@ -236,42 +164,6 @@ export const songs: Song[] = [
         "artists": ["Tenno"],
         "album": "Study Session",
         "duration": "3:40"
-    },
-    {
-        "id": 2,
-        "albumId": 4,
-        "title": "Go go go!",
-        "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "3:20"
-    },
-    {
-        "id": 3,
-        "albumId": 4,
-        "title": "Keep focus!",
-        "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "2:40"
-    },
-    {
-        "id": 4,
-        "albumId": 4,
-        "title": "JavaScript is the way",
-        "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "3:10"
-    },
-    {
-        "id": 5,
-        "albumId": 4,
-        "title": "No more TypeScript for me",
-        "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
-        "artists": ["Tenno"],
-        "album": "Study Session",
-        "duration": "2:10"
     },
     {
         "id": 1,
@@ -283,35 +175,8 @@ export const songs: Song[] = [
         "duration": "3:12"
     },
     {
-        "id": 2,
-        "albumId": 5,
-        "title": "Coffee Daze",
-        "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
-        "artists": ["LoFi Dreamer"],
-        "album": "Chill Lo-Fi Music",
-        "duration": "4:07"
-    },
-    {
-        "id": 3,
-        "albumId": 5,
-        "title": "Skyline Serenade",
-        "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
-        "artists": ["LoFi Dreamer"],
-        "album": "Chill Lo-Fi Music",
-        "duration": "3:50"
-    },
-    {
-        "id": 4,
-        "albumId": 5,
-        "title": "Urban Echoes",
-        "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
-        "artists": ["LoFi Dreamer"],
-        "album": "Chill Lo-Fi Music",
-        "duration": "3:30"
-    },
-    {
-        "id": 5,
-        "albumId": 5,
+        "id": 1,
+        "albumId": 6,
         "title": "Night's End",
         "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
         "artists": ["LoFi Dreamer"],
